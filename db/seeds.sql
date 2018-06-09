b457a050c2b23c75a77af873d6efae8f7f9823fe
Andrea <Andrea@Macintosh.attlocal.net>
2018-06-09 00:02:36 -0500

    almost done

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("goat cheese burger", false);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("southwest burger", false);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("siracha burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("american burger", false);
